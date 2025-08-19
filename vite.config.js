9de088b419b127b9f46d59341ff91b314c623207
nazmul hossen <140499399+nazmul1150@users.noreply.github.com>
2025-08-19 22:22:12 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import path from 'path';
-
+// // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   build: {
-//     outDir: path.resolve(__dirname, '../electric-lineman/build'),
-//     emptyOutDir: true,
-//     rollupOptions: {
-//       input: '/src/main.jsx',
-//       output: {
-//         entryFileNames: 'index.js',
-//         assetFileNames: 'style.css'
-//       }
-//     }
-//   }
-// });
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, '../electric-lineman/build'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: '/src/main.jsx',
+      output: {
+        entryFileNames: 'index.js',
+        assetFileNames: 'style.css'
+      }
+    }
+  }
+});
 
